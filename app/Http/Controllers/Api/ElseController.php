8d5248c0a97f25a386983859ae2b9d0c5dd04b32
chenchen <1452073959@qq.com>
2020-09-26 22:05:06 +0800

    1

--- app/Http/Controllers/Api/ElseController.php
+++ app/Http/Controllers/Api/ElseController.php
@@ -20,29 +20,63 @@ class ElseController extends Controller
         return $this->success($data);
     }
 
+//    public function box(Request $request)
+//    {
+//        $rand = rand(10, 30);
+//        if ($request->has('pid')) {
+//            $flight = User::find(request('pid'));
+//            $flight->diamondnum = ($flight['diamondnum'] += 0) + $rand;
+//            $flight->save();
+//        };
+//        $user = auth('api')->user();
+//        if ($user['box'] <= 0) {
+//            return $this->success('不能再开了');
+//        }
+//        if (!$request->has('pid')) {
+//            $user->box = $user['box'] - 1;
+//            $user->save();
+//            return  $this->success(['boxnum' => $user['box'], 'rand' => $rand]);
+//        };
+//        if($request->has('and')){
+//            $user->box = $user['box'] - 1;
+//            $user->save();
+//            return  $this->success(['boxnum' => $user['box'], 'rand' => $rand]);
+//        }
+//
+//    }
+
     public function box(Request $request)
     {
-        $rand = rand(10, 30);
+        $rand = rand(50, 100);
+        $rand1 = rand(100, 150);
+        $user = auth('api')->user();
+        //开启分享箱子
         if ($request->has('pid')) {
+            //分享人增加
             $flight = User::find(request('pid'));
             $flight->diamondnum = ($flight['diamondnum'] += 0) + $rand;
             $flight->save();
+            //开箱人增加
+            $user->diamondnum = ($user['diamondnum'] += 0) + $rand1;
+            $user->save();
+            return $this->success(['rand' => $rand]);
         };
-        $user = auth('api')->user();
         if ($user['box'] <= 0) {
             return $this->success('不能再开了');
         }
-        if (!$request->has('pid')) {
-            $user->box = $user['box'] - 1;
-        };
+        //发起分享箱子  减次数 不加钻石
         if($request->has('and')){
             $user->box = $user['box'] - 1;
+            $user->save();
+            return  $this->success(['boxnum' => $user['box']]);
         }
-        $user->save();
-
-//        dd($boxnum->toarray());
-
-        return $this->success(['boxnum' => $user['box'], 'rand' => $rand]);
+        //免费第一次箱子
+        if($user['box']==6){
+            $user->box = $user['box'] - 1;
+            $user->diamondnum = ($user['diamondnum'] += 0) + $rand;
+            $user->save();
+            return  $this->success(['boxnum' => $user['box'], 'rand' => $rand]);
+        };
     }
 
     public function draw()

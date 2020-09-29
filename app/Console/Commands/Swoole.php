d68af37c0e933a60ef6a940c8cc059a35e40b406
chenchen <1452073959@qq.com>
2020-09-29 14:32:47 +0800

    1

--- app/Console/Commands/Swoole.php
+++ app/Console/Commands/Swoole.php
@@ -6,10 +6,20 @@ use Illuminate\Console\Command;
 
 class Swoole extends Command
 {
-    protected $signature = 'swoole:demo';
-    protected $description = '这是关于swoole的一个测试demo';
-    private static $server = null;
+    public $ws;
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'swoole {action?}';
 
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'swoole';
 
     /**
      * Create a new command instance.
@@ -24,74 +34,87 @@ class Swoole extends Command
     /**
      * Execute the console command.
      *
-     * @return int
+     * @return mixed
      */
     public function handle()
     {
-        $server = self::getWebSocketServer();
-        $server->on('open',[$this,'onOpen']);
-        $server->on('message', [$this, 'onMessage']);
-        $server->on('close', [$this, 'onClose']);
-        $server->on('request', [$this, 'onRequest']);
-        $this->line("swoole服务启动成功 ...");
-        $server->start();
-    }
-
-    // 获取服务
-    public static function getWebSocketServer()
-    {
-        if (!(self::$server instanceof \swoole_websocket_server)) {
-            self::setWebSocketServer();
-        }
-        return self::$server;
-    }
-    // 服务处始设置
-    protected static function setWebSocketServer():void
-    {
-        self::$server = new \swoole_websocket_server("0.0.0.0", 9600);
-        self::$server->set([
-            'worker_num' => 1,
-            'heartbeat_check_interval' => 60, // 60秒检测一次
-            'heartbeat_idle_time' => 121, // 121秒没活动的
-        ]);
-    }
-
-    // 打开swoole websocket服务回调代码
-    public function onOpen($server, $request)
-    {
-        if ($this->checkAccess($server, $request)) {
-            self::$server->push($request->fd,"打开swoole服务成功！");
+        $action = $this->argument('action');
+        switch ($action) {
+            case 'restart':
+                $this->info('swoole server restarted');
+                break;
+            case 'close':
+                $this->info('swoole server stoped');
+                break;
+            default:
+                $this->start();
+                break;
         }
     }
-    // 给swoole websocket 发送消息回调代码
-    public function onMessage($server, $frame)
-    {
-        $server->push($frame->fd, '第几个用户给我发送'.$frame->fd);
-    }
 
-    // websocket 关闭回调代码
-    public function onClose($serv,$fd)
-    {
-        $this->line("客户端 {$fd} 关闭");
-    }
-    // 校验客户端连接的合法性,无效的连接不允许连接
-    public function checkAccess($server, $request):bool
-    {
-        $bRes = true;
-        if (!isset($request->get) || !isset($request->get['token'])) {
-            self::$server->close($request->fd);
-            $this->line("接口验证字段不全");
-            $bRes = false;
-        } else if ($request->get['token'] !== "123456") {
-            $this->line("接口验证错误");
-            $bRes = false;
-        }
-        return $bRes;
-    }
-    // 启动websocket服务
     public function start()
     {
-        self::$server->start();
+        //创建websocket服务器对象，监听0.0.0.0:9501端口
+        $this->ws = new \swoole_websocket_server("0.0.0.0", 9501);
+
+        //开启ssl模式
+//        $this->ws = new \swoole_websocket_server("0.0.0.0", 9501,SWOOLE_PROCESS, SWOOLE_SOCK_TCP | SWOOLE_SSL);
+
+        //配置ssl模式
+//        $this->ws->set([
+//            'ssl_key_file' => '/var/www/html/ssl/4033464_cxh520.top.key',
+//            'ssl_cert_file' => '/var/www/html/ssl/4033464_cxh520.top.pem'
+//        ]);
+
+        //监听WebSocket连接打开事件
+        $this->ws->on('open',[$this,'open']);
+
+        //监听WebSocket消息事件
+        $this->ws->on('message',[$this,'message']);
+
+        //监听WebSocket主动推送消息事件
+        $this->ws->on('request',[$this,'request']);
+
+        //监听WebSocket连接关闭事件
+        $this->ws->on('close',[$this,'close']);
+
+        $this->ws->start();
+    }
+
+    /**
+     * 建立连接
+     * @param $ws
+     * @param $request
+     */
+    public function open($ws, $request){
+        var_dump($request->fd . "连接成功");
+    }
+
+    /**
+     * 接收消息
+     * @param $ws
+     * @param $frame
+     */
+    public function message($ws,$frame){
+        var_dump('发送成功');
+    }
+
+    /**
+     * 接收请求
+     * @param $request
+     * @param $response
+     */
+    public function request($request,$response){
+
+    }
+
+    /**
+     * 关闭连接
+     * @param $ws
+     * @param $fd
+     */
+    public function close($ws,$fd){
+
     }
 }
 
